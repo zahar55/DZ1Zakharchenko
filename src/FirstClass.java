a9da37920abdc8e38e554e441701cdc9fef0d490
Andrey <skyitcompany@gmail.com>
2016-08-14 20:18:26 +0300

    DZ1

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,6 +2,6 @@ public class FirstClass {
 
      static void main(String[] args) {
 
-         System.out.printf("MyFirstClassOnJava");
+         System.out.printf("My First Class On Java");
     }
 }
